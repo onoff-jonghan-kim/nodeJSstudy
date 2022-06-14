65ac2ceef9d79e0769844f0ac83e834a7f2714cc
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-06-14 13:02:52 +0900

    생활코딩 node.js-27

--- main.js
+++ main.js
@@ -2,38 +2,44 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+const templateHTML = (title, list, description) => {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <h2>${title}</h2>
+    <p>${description}</p>
+  </body>
+  </html>
+  `
+}
+const templateList = (files) =>{
+  let list = `<ul>`;
+  files.forEach(file => {
+    list = list + `<li><a href="/?id=${file}">${file}</a></li>`
+  })
+  list = list+`</ul>`;
+  return list
+}
+
+const app = http.createServer((request,response) => {
+    const _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
 
     if( pathname == '/'){
-      fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description= "Hello! Node.js"){
+      fs.readFile(`data/${queryData.id}`, 'utf-8', (err, description= "Hello! Node.js") => {
         fs.readdir("./data/", (err, files) => {
-            let title = queryData.id;
-            if(title === undefined){
-                title = 'Welcome';
-            }
-            let list = `<ul>`;
-            files.forEach(file => {
-              list = list + `<li><a href="/?id=${file}">${file}</a></li>`
-            })
-            list = list+`</ul>`;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+          let title = queryData.id;
+          if(title === undefined){ title = 'Welcome'; }
+          let list = templateList(files);
+          const template = templateHTML(title, list, description);
           response.writeHead(200);
           response.end(template);
         });
