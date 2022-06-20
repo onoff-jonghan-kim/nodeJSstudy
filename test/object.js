48207acab2ebe0a0e43f312ad19bd51442e5aea8
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-06-20 11:20:04 +0900

    object loop

---
+++ test/object.js
@@ -0,0 +1,9 @@
+const roles = {
+  'programer' : 'egoing',
+  'designer' : 'k8805',
+  'manager' : 'jong'
+}
+
+for(let name in roles){
+  console.log('object -> ', name, "value -> ", roles[name]);
+}
